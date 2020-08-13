31628e59b5cec80eecac8dc2022402e928fba8a5
Blueberryg <67917884+Blueberryg@users.noreply.github.com>
2020-08-13 04:15:41 +0300

    add for heroju
    believe

---
+++ Total.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Результаты</title>
+    </head>
+    <body>
+        <?php
+        $tot = ["Камиль","Адыл","Ринат"];
+        shuffle($tot);
+        echo $tot;
+        ?>
+    </body>
+</html>
